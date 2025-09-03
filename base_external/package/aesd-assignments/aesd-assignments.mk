1183570af5e6980a192398d87d059841b696865f
Christian Bull <cbull.projects@gmail.com>
2025-09-02 22:00:13 -0600

    update buildroot config, add clean.sh

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 098063717dc6d89ac62b954a1df4d5b0492af5f6
+AESD_ASSIGNMENTS_VERSION = b8820481e561d1e1ce3dc9f575280d4aff7fe6ae
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -22,8 +22,9 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/finder-app/* $(TARGET_DIR)/bin
+	cp -r $(@D)/finder-app/* $(TARGET_DIR)/bin/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4-buildroot/* $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
