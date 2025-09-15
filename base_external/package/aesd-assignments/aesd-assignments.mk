895f98c8b4c9fb2b9f14235336148437b5d75085
Christian Bull <cbull.projects@gmail.com>
2025-09-15 11:47:06 -0600

    update version for external assignment repo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = b2fb109e003fca6eb120cfe0a6cdcc28cb5c4272
+AESD_ASSIGNMENTS_VERSION = 7b0015bf27673b0362f11cca5fbcde7d6e0e6933
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
