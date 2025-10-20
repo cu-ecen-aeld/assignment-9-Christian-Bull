e670ea379041cc380da48484cc2a526b722fa51d
Christian Bull <cbull.projects@gmail.com>
2025-10-20 16:08:39 +0000

    update config with debugging

--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_CHAR_DRIVER_VERSION = f15084656d668b88ae58a3002361a192327e0e0c
+AESD_CHAR_DRIVER_VERSION = 28a0ecf7322cd734de55d1d72a75b15e7d53899f
 AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Christian-Bull.git
 AESD_CHAR_DRIVER_SITE_METHOD = git
 # AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
